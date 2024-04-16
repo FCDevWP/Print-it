d5b39b9ee3e8585253c54c1150b3403361e19eaa
Frederic Claveau <fredclaveau@gmail.com>
2024-04-16 22:59:49 +0200

    ajout addeventlistener sur les flèches

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,15 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+const baliseFG = document.getElementById("fleche_gauche")
+console.log(baliseFG);
+baliseFG.addEventListener("click", () => {
+	console.log("Vous avez cliqué sur la flèche gauche")
+});
+
+const baliseFD = document.getElementById("fleche_droite")
+console.log(baliseFD);
+baliseFD.addEventListener("click", () => {
+	console.log("Vous avez cliqué sur la flèche droite")
+});
